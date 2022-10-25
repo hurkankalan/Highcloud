4d1828aa47046496589ae844c7b9651830ad1e37
Hurkan Kalan <hurkan.kalan@gmail.com>
2022-10-25 22:34:03 +0200

    add take picture button and icon and resize it

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,12 +1,75 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const CameraScreen = () => {
+  const [cameraType, setCameraType] = useState(CameraType.back);
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (!hasPermission) {
+    return (
+      <Text style={styles.alertText}>
+        No access and authorization to camera
+      </Text>
+    );
+  }
+
+  function toggleCameraType() {
+    setCameraType(
+      cameraType === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
   return (
-    <View>
-      <Text>Camera Screen</Text>
-    </View>
+    <Camera style={styles.camera} type={cameraType} ref={cameraRef}>
+      <View style={styles.cameracontainer}>
+        <TouchableOpacity
+          style={styles.reverseCamerabutton}
+          onPress={toggleCameraType}
+          activeOpacity={0.4}
+        >
+          <Ionicons name="camera-reverse-sharp" size={50} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.1}
+          style={styles.takePicturesButtons}
+        >
+          <Ionicons name="radio-button-off-outline" size={100} color="white" />
+        </TouchableOpacity>
+      </View>
+    </Camera>
   );
 };
 
 export default CameraScreen;
+
+const styles = StyleSheet.create({
+  camera: {
+    flex: 1,
+  },
+  cameracontainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+  reverseCamerabutton: {
+    position: "absolute",
+    right: 10,
+    padding: 10,
+  },
+  takePicturesButtons: {
+    position: "absolute",
+    top: 620,
+  },
+  alertText: {
+    textAlign: "center",
+  },
+});
